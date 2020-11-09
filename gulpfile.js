2b46f4756de31a2f80b3dec8ad76fd4b5c5af611
kovalenko.olexandra <kovalenko.olexandra@gmail.com>
2020-11-09 20:59:14 +0200

    start

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+'use strict';
+ 
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+ 
+sass.compiler = require('node-sass');
+ 
+gulp.task('sass', function () {
+  return gulp.src('./sass/**/*.scss')
+    .pipe(sass.sync().on('error', sass.logError))
+    .pipe(gulp.dest('./css'));
+});
+ 
+gulp.watch('./sass/**/*.scss', gulp.parallel('sass'));
